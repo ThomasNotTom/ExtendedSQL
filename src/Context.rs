581d463340bcd9ec1b0a45d67c034071eb4817d0
Thomas <31802793+ThomasNotTom@users.noreply.github.com>
2024-11-27 12:07:02 +0000

    Create Context.rs

---
+++ src/Context.rs
@@ -0,0 +1,22 @@
+use crate::Database;
+use std::collections::BTreeMap;
+
+pub struct Context {
+    pub databases: BTreeMap<String, Box<Database::Database>>,
+}
+
+impl Default for Context {
+    fn default() -> Self {
+        Context {
+            databases: BTreeMap::<String, Box<Database::Database>>::new(),
+        }
+    }
+}
+
+impl Context {
+    pub fn new(database: BTreeMap<String, Box<Database::Database>>) -> Self {
+        Context {
+            databases: database,
+        }
+    }
+}
