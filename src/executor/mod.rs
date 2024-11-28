c20acc144d71ec38e96574274df8882ea7ba72b4
Thomas <31802793+ThomasNotTom@users.noreply.github.com>
2024-11-28 16:53:29 +0000

    Create create table function

--- src/executor/mod.rs
+++ src/executor/mod.rs
@@ -4,11 +4,24 @@ use std::collections::BTreeMap;
 use crate::{
     context::Context,
     database::Database,
-    parsing::{ContextElement, CreateDatabase},
+    parsing::{ContextElement, CreateDatabase, CreateTable},
+    table::Table,
 };
 
+pub fn execute_create_table(context: &mut Context, data: CreateTable) {
+    let table_name = &data.table_name;
+    let database_name = &data.database_name;
+
+    let mut database = context
+        .databases
+        .get_mut(database_name)
+        .expect("")
+        .borrow_mut();
+    database.add_table(table_name.to_string(), RefCell::new(Table::default()));
+}
+
 pub fn execute_create_database(context: &mut Context, data: CreateDatabase) {
-    let name = &data.database_name.to_string();
+    let name = &data.name.to_string();
     context.databases.insert(
         name.to_string(),
         RefCell::new(Database::new(name.to_string(), BTreeMap::new())),
@@ -18,6 +31,7 @@ pub fn execute_create_database(context: &mut Context, data: CreateDatabase) {
 pub fn execute_line(context: &mut Context, element: ContextElement) {
     match element {
         ContextElement::CreateDatabase(data) => execute_create_database(context, data),
+        ContextElement::CreateTable(data) => execute_create_table(context, data),
     }
 }
 
