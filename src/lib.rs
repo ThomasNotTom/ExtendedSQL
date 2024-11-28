16ebf57de41d08390c36401e842caf1704382354
Thomas <31802793+ThomasNotTom@users.noreply.github.com>
2024-11-28 12:18:05 +0000

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,7 @@
-#[warn(non_snake_case)]
-pub mod Context;
-
-#[warn(non_snake_case)]
-pub mod Database;
-
 pub mod cell;
 pub mod constraint;
+pub mod context;
+pub mod database;
 pub mod header;
 pub mod row;
 pub mod table;
