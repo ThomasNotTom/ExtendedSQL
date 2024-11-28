5318542b876500cb05ae05763e64b08f13dbf802
Thomas <31802793+ThomasNotTom@users.noreply.github.com>
2024-11-28 15:35:43 +0000

    Change to RefCell

--- src/executor/mod.rs
+++ src/executor/mod.rs
@@ -1,21 +1,37 @@
-use std::{collections::BTreeMap, rc::Rc};
+use std::{
+    borrow::Borrow,
+    cell::{Ref, RefCell},
+    collections::BTreeMap,
+    ops::Deref,
+    rc::Rc,
+};
 
 use crate::{
     context::Context,
     database::Database,
-    parsing::{ContextElement, CreateDatabase},
+    parsing::{ContextElement, CreateDatabase, CreateTable},
+    table::Table,
 };
 
 pub fn execute_create_database(context: &mut Context, data: CreateDatabase) {
+    let name = &data.name;
     context.databases.insert(
-        data.database_name,
-        Rc::new(Database::new(data.database_name, BTreeMap::new())),
+        name.clone(),
+        RefCell::new(Database::new(name.clone(), BTreeMap::new())),
     );
+}
+
+pub fn execute_create_table(context: &mut Context, data: CreateTable) {
+    let table_name: String = data.table_name;
+    let database_name: &String = &data.database_name;
+    let database = context.databases.get_mut(database_name).expect("");
+    database.add_table(table_name, RefCell::new(Table::default()));
 }
 
 pub fn execute_line(context: &mut Context, element: ContextElement) {
     match element {
         ContextElement::CreateDatabase(data) => execute_create_database(context, data),
+        ContextElement::CreateTable(data) => execute_create_table(context, data),
     }
 }
 
