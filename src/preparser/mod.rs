6579f210761deed085c44a82f5c26bec0226e189
Thomas <31802793+ThomasNotTom@users.noreply.github.com>
2024-11-29 01:13:34 +0000

    Include parsing of brackets

--- src/preparser/mod.rs
+++ src/preparser/mod.rs
@@ -26,6 +26,7 @@ pub fn keyword_to_string(string: Keywords) -> String {
 pub enum PreParseData {
     Keyword(Keywords),
     UserText(String),
+    NormalBracketText(String),
 }
 
 pub fn pre_parse(line: String) -> Vec<PreParseData> {
@@ -34,25 +35,54 @@ pub fn pre_parse(line: String) -> Vec<PreParseData> {
     let chars = line.chars().collect::<Vec<char>>();
     let mut buffer = String::new();
 
+    let mut bracket_depth = 0;
+    let mut inner_bracket_text = String::new();
+
     for char in chars {
-        if char == ' ' {
-            let matched_keyword = string_to_keyword(&buffer.clone());
-            if matched_keyword.is_none() {
-                pre_parsed.push(PreParseData::UserText(buffer.clone()));
-            } else {
-                pre_parsed.push(PreParseData::Keyword(matched_keyword.expect("")));
+        match char {
+            ' ' => {
+                if bracket_depth != 0 {
+                    continue;
+                }
+                let matched_keyword = string_to_keyword(&buffer.clone());
+                if matched_keyword.is_none() {
+                    pre_parsed.push(PreParseData::UserText(buffer.clone()));
+                } else {
+                    pre_parsed.push(PreParseData::Keyword(matched_keyword.expect("")));
+                }
+                buffer = String::new();
+                continue;
             }
-            buffer = String::new();
-            continue;
+            '(' => {
+                bracket_depth += 1;
+                continue;
+            }
+            ')' => {
+                bracket_depth -= 1;
+                if bracket_depth == 0 {
+                    pre_parsed.push(PreParseData::NormalBracketText(inner_bracket_text.clone()));
+                    inner_bracket_text.clear();
+                }
+                continue;
+            }
+            _ => {
+                if bracket_depth == 0 {
+                    buffer.push(char);
+                } else {
+                    inner_bracket_text.push(char);
+                }
+                continue;
+            }
         }
-        buffer.push(char);
     }
 
-    let matched_keyword = string_to_keyword(&buffer.clone());
-    if matched_keyword.is_none() {
-        pre_parsed.push(PreParseData::UserText(buffer.clone()));
-    } else {
-        pre_parsed.push(PreParseData::Keyword(matched_keyword.expect("")));
+    if &buffer.clone() != "" {
+        let matched_keyword = string_to_keyword(&buffer.clone());
+        if matched_keyword.is_none() {
+            pre_parsed.push(PreParseData::UserText(buffer.clone()));
+        } else {
+            pre_parsed.push(PreParseData::Keyword(matched_keyword.expect("")));
+        }
     }
 
     pre_parsed
